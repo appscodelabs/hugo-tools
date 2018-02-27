a365ff6141e325a77e72ff4ff2358145776b1799
tamal <tamal@appscode.com>
2018-02-27 08:44:33 -0800

    added-all

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gohugoio/hugo/parser"
-	"strings"
 )
 
 func main() {
+	addFrontMatter()
+}
+
+func addFrontMatter() {
 	args := os.Args
 	if len(args) < 2 {
 		log.Fatalln("missing directory name")
@@ -28,20 +34,59 @@ func main() {
 				return nil
 			}
 
-			filename := path
-			f, err := os.Open(filename)
+			data, err := ioutil.ReadFile(path)
 			if err != nil {
 				return err
 			}
-			defer f.Close()
+			buf := bytes.NewBuffer(data)
 
-			page, err := parser.ReadFrom(f)
+			page, err := parser.ReadFrom(buf)
 			if err != nil {
 				return err
 			}
 			fm := page.FrontMatter()
 			if len(fm) == 0 {
-				fmt.Println(filename)
+				fmt.Println(path)
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("error walking the path %q: %v\n", dir, err)
+		}
+	}
+}
+
+func checkFrontMatter() {
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatalln("missing directory name")
+	}
+	for _, dir := range os.Args[1:] {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", dir, err)
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			if !strings.HasSuffix(path, ".md") {
+				return nil
+			}
+
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			buf := bytes.NewBuffer(data)
+
+			page, err := parser.ReadFrom(buf)
+			if err != nil {
+				return err
+			}
+			fm := page.FrontMatter()
+			if len(fm) == 0 {
+				fmt.Println(path)
 			}
 			return nil
 		})
