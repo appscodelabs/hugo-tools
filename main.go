b2d01ebd391bc64e034269b18f5269ac539cf0fa
tamal <tamal@appscode.com>
2018-03-08 17:41:06 -0800

    added-all

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	addFrontMatter()
+	introListFrontMatter()
 }
 
 var (
@@ -155,6 +155,53 @@ func checkFrontMatter() {
 			if len(fm) == 0 {
 				fmt.Println(path)
 			}
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("error walking the path %q: %v\n", dir, err)
+		}
+	}
+}
+
+func introListFrontMatter() {
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatalln("missing directory name")
+	}
+	for _, dir := range os.Args[1:] {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", dir, err)
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			if !strings.HasSuffix(path, ".md") {
+				return nil
+			}
+
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			buf := bytes.NewBuffer(data)
+
+			page, err := parser.ReadFrom(buf)
+			if err != nil {
+				return err
+			}
+
+			var b2 bytes.Buffer
+			b2.Write(page.FrontMatter())
+			c := page.Content()
+			bytes.TrimSpace(c)
+			b2.WriteString("> New to Voyager? Please start [here](/docs/concepts/overview.md).")
+			b2.WriteRune('\n')
+			b2.WriteRune('\n')
+			b2.Write(c)
+			ioutil.WriteFile(path, b2.Bytes(), 0755)
+
 			return nil
 		})
 		if err != nil {
