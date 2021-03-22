bd1459157650f0c2108b8676048def8de63280c5
Tamal Saha <tamal@appscode.com>
2021-03-22 12:26:27 -0700

    Add update-branch command
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ cmds/update_branch.go
@@ -0,0 +1,85 @@
+/*
+Copyright AppsCode Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmds
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	saapi "github.com/appscode/static-assets/api"
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/yaml"
+)
+
+var filename string
+var branch string
+
+func NewCmdUpdateBranch() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "update-branch",
+		Short:             "Update branch name for latest version",
+		DisableAutoGenTag: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return updateBranch()
+		},
+	}
+	cmd.Flags().StringVar(&filename, "filename", filename, "Path to product file")
+	cmd.Flags().StringVar(&branch, "branch", branch, "Product branch or git commit sha or git tag")
+	return cmd
+}
+
+func updateBranch() error {
+	if !Exists(filename) {
+		// Avoid missing product files like stash-catalog key
+		return nil
+	}
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	var prod saapi.Product
+	err = yaml.Unmarshal(data, &prod)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(prod.LatestVersion)
+	for i, v := range prod.Versions {
+		if v.Version == prod.LatestVersion {
+			v.Branch = branch
+			prod.Versions[i] = v
+			break
+		}
+	}
+
+	data, err = json.MarshalIndent(prod, "", "  ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+// Exists reports whether the named file or directory Exists.
+func Exists(name string) bool {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
